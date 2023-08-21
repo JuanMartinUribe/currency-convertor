11629a9701e8da2d25b450778bf148798bc6af16
JuanMartinUribe <jmuribef@eafit.edu.co>
2023-08-21 13:26:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [initialCurrency, setInitialCurrency] = useState("USD");
+  const [goalCurrency, setGoalCurrency] = useState("USD");
+  const [result, setResult] = useState("");
+
+  useEffect(() => {
+    console.log("use effect called");
+    if (initialCurrency === goalCurrency || !value) {
+      setResult(() => value);
+      return;
+    }
+    const fetchData = async () => {
+      const API_URL = `https://api.frankfurter.app/latest?amount=${value}&from=${initialCurrency}&to=${goalCurrency}`;
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setResult(data.rates[goalCurrency]);
+    };
+
+    fetchData();
+  }, [initialCurrency, goalCurrency, value]);
+
+  const handleInputChange = function (e) {
+    const newValue = +e.target.value;
+    if (isNaN(newValue)) return;
+    setValue(newValue);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={value} onChange={handleInputChange} />
+      <select
+        value={initialCurrency}
+        onChange={(e) => setInitialCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={goalCurrency}
+        onChange={(e) => setGoalCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+      {value &&
+        `${value} ${initialCurrency} is equivalent to ${result} ${goalCurrency}`}
     </div>
   );
 }
